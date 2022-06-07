854c713480565f52b470ded0270f8b6bdeafcfb7
Jordan McKinney <jordanmmck@gmail.com>
2022-06-07 13:54:48 -0700

    update avax script

--- avax/coingecko.py
+++ avax/coingecko.py
@@ -3,19 +3,26 @@ import pandas as pd
 import csv
 import datetime
 import json
+import time
 
-avg_gas_prices = pd.read_csv('./data/avax/export-AvgGasPrice.csv', index_col=0)
-gas_used = pd.read_csv('./data/avax/export-GasUsed.csv', index_col=0)
+start_date = datetime.date(2022, 1, 1)
+end_date = datetime.date(2022, 6, 1)
+time_diff = end_date-start_date
+time_diff_days = time_diff.days
+
+avg_gas_prices = pd.read_csv(
+    './data/avax/export-AvgGasPrice.csv', index_col=None)
+gas_used = pd.read_csv('./data/avax/export-GasUsed.csv', index_col=None)
 block_rewards = pd.read_csv(
-    './data/avax/export-BlockCountRewards.csv', index_col=0)
-fee_burn = pd.read_csv('./data/avax/export-DailyBurnt.csv', index_col=0)
-tx_count = pd.read_csv('./data/avax/export-TxGrowth.csv', index_col=0)
+    './data/avax/export-BlockCountRewards.csv', index_col=None)
+fee_burn = pd.read_csv('./data/avax/export-DailyBurnt.csv', index_col=None)
+tx_count = pd.read_csv('./data/avax/export-TxGrowth.csv', index_col=None)
 
 super_frame = avg_gas_prices.assign(
     rewards=gas_used.Value, block_rewards=block_rewards.Value, fee_burn=tx_count.Value)
 
-super_frame.columns = [
-    'unix_time', 'avg_gas_price_wei', 'gas_used', 'block_rewards_avax', 'tx_count']
+super_frame.columns = ['date', 'unix_time', 'avg_gas_price_wei',
+                       'gas_used', 'block_rewards_avax', 'tx_count']
 
 block_reward_sum = 0
 block_reward_sum_usd = 0
@@ -29,9 +36,19 @@ WEI_TO_AVAX = 1000000000000000000
 
 daily_fees = []
 
-for i, row in super_frame.tail(30).iterrows():
-    date = datetime.datetime.fromtimestamp(
-        row['unix_time']).strftime('%d-%m-%Y')
+start_index = -1
+end_index = -1
+
+start_index = super_frame[super_frame.date ==
+                          start_date.strftime('%-m/%-d/%Y')].index[0]
+end_index = super_frame[super_frame.date ==
+                        end_date.strftime('%-m/%-d/%Y')].index[0]
+
+super_frame = super_frame.iloc[start_index:end_index+1]
+
+for i, row in super_frame.iterrows():
+    date = row['date'].split('/')
+    date = '-'.join([date[1], date[0], date[2]])
 
     avax_price_res = requests.get(
         'https://api.coingecko.com/api/v3/coins/avalanche-2/history?date={}'.format(date))
@@ -60,13 +77,27 @@ for i, row in super_frame.tail(30).iterrows():
 
     tx_count_sum += int(row['tx_count'])
 
+    # prevent coingecko blocking us
+    time.sleep(1.1)
+
+
 # get fee burn data
 fee_burn_data = csv.DictReader(open('./data/avax/export-DailyBurnt.csv'))
 fee_burn_dict = [d for d in fee_burn_data]
-last_30_days_fee_burn = fee_burn_dict[-30:]
+
+start_index = -1
+end_index = -1
+
+for index, day_object in enumerate(fee_burn_dict):
+    if day_object.get('Date(UTC)') == start_date.strftime('%m/%d/%Y'):
+        start_index = index
+    elif day_object.get('Date(UTC)') == end_date.strftime('%m/%d/%Y'):
+        end_index = index
+
+last_n_days_fee_burn = fee_burn_dict[start_index:end_index+1]
 
 
-for day_fee_burn, day_daily_fees in zip(last_30_days_fee_burn, daily_fees):
+for day_fee_burn, day_daily_fees in zip(last_n_days_fee_burn, daily_fees):
     fee_burn = float(day_fee_burn.get('BurntFees'))
     fee_burn_sum += float(fee_burn)
 
@@ -86,38 +117,42 @@ for day_fee_burn, day_daily_fees in zip(last_30_days_fee_burn, daily_fees):
     day_daily_fees['avg_fees_paid_to_miners_usd'] = day_daily_fees.get(
         'approx_fees_paid_to_miners_usd') / day_daily_fees.get('tx_count')
 
+    time.sleep(1.1)
+
 file = open('data/avax/avax.json', 'w')
 file.write(json.dumps(daily_fees))
 file.close()
 
-print('30 day total tx count: {}'.format(tx_count_sum))
+print('total tx count: {:,}'.format(tx_count_sum))
 
 print()
 
-print('30 day total block reward sum (AVAX): {}'.format(block_reward_sum))
-print('30 day average block reward (AVAX): {}'.format(block_reward_sum/30))
+print('total block reward sum (AVAX): {:,}'.format(block_reward_sum))
+print('daily average block reward (AVAX): {:,}'.format(
+    block_reward_sum/time_diff_days))
 
 print()
 
-print('30 day total block rewards (USD): ${}'.format(block_reward_sum_usd))
-print('30 day average block rewards (USD): ${}'.format(block_reward_sum_usd/30))
+print('total block rewards (USD): ${:,}'.format(block_reward_sum_usd))
+print('daily average block rewards (USD): ${:,}'.format(
+    block_reward_sum_usd/time_diff_days))
 
 print()
 
-print('30 day total fees (AVAX): {}'.format(fee_sum))
-print('30 day average fees (AVAX): {}'.format(fee_sum/30))
+print('total fees (AVAX): {:,}'.format(fee_sum))
+print('daily average fees (AVAX): {:,}'.format(fee_sum/time_diff_days))
 
 print()
 
-print('30 day total fees (USD): ${}'.format(fee_sum_usd))
-print('30 day average fees (USD): ${}'.format(fee_sum_usd/30))
+print('total fees (USD): ${:,}'.format(fee_sum_usd))
+print('daily average fees (USD): ${:,}'.format(fee_sum_usd/time_diff_days))
 
 print()
 
-print('30 day total burn (AVAX): {}'.format(fee_burn_sum))
-print('30 day average burn (AVAX): {}'.format(fee_burn_sum/30))
+print('total burn (AVAX): {:,}'.format(fee_burn_sum))
+print('daily average burn (AVAX): {:,}'.format(fee_burn_sum/time_diff_days))
 
 print()
 
-print('30 day total burn (USD): ${}'.format(fee_burn_usd))
-print('30 day average burn (USD): ${}'.format(fee_burn_usd/30))
+print('total burn (USD): ${:,}'.format(fee_burn_usd))
+print('daily average burn (USD): ${:,}'.format(fee_burn_usd/time_diff_days))
--- sol/coingecko.py
+++ sol/coingecko.py
@@ -1,10 +1,17 @@
 import json
 import requests
+import time
+import datetime
 
-last_30_days = []
+start_date = datetime.date(2022, 1, 1)
+end_date = datetime.date(2022, 6, 1)
+time_diff = end_date-start_date
+time_diff_days = time_diff.days
+
+last_n_days = []
 
 # https://www.stakingrewards.com/earn/solana/metrics/
-SOL_ANNUAL_INFLATION_RATE = 0.0722
+SOL_ANNUAL_INFLATION_RATE = 0.069
 daily_inflation_rate = (1 + SOL_ANNUAL_INFLATION_RATE) ** (1 / 365)
 
 supply_res = requests.get('https://api.coingecko.com/api/v3/coins/solana/')
@@ -15,7 +22,7 @@ intermediate_supply = total_supply
 issuance_sum = 0
 issuance_sum_usd = 0
 
-for i in range(30):
+for i in range(time_diff_days):
     day_issuance = intermediate_supply - intermediate_supply / daily_inflation_rate
     issuance_sum += day_issuance
     intermediate_supply -= day_issuance
@@ -23,11 +30,11 @@ for i in range(30):
 with open('data/sol/sol.json', 'r') as f:
     data = json.load(f)
     tx_data = data.get('solana').get('transactions')
-    last_30_days = tx_data[-30:]
+    last_n_days = tx_data[-time_diff_days:]
 
 tx_sum_sol = 0
 tx_sum_usd = 0
-for day in last_30_days:
+for day in last_n_days:
     daily_fee_sol = day.get('transactionFee')
     tx_sum_sol += daily_fee_sol
 
@@ -47,21 +54,26 @@ for day in last_30_days:
     issuance_sum_usd += daily_issuance * daily_price
     intermediate_supply += daily_issuance
 
+    # prevent getting blocked by coingecko
+    time.sleep(1)
 
-print('30 day total tx fee sum (SOL): {}'.format(tx_sum_sol))
-print('30 day average (SOL): {}'.format(tx_sum_sol/30))
 
-print()
-
-print('30 day total tx fee sum (USD): ${}'.format(tx_sum_usd))
-print('30 day average (USD): ${}'.format(tx_sum_usd/30))
+print('total tx fee sum (SOL): {:,}'.format(tx_sum_sol))
+print('tx fee daily average (SOL): {:,}'.format(tx_sum_sol/time_diff_days))
 
 print()
 
-print('30 day total issuance (SOL): {}'.format(issuance_sum))
-print('30 day average issuance (SOL): {}'.format(issuance_sum/30))
+print('total tx fee sum (USD): ${:,}'.format(tx_sum_usd))
+print('average tx fee (USD): ${:,}'.format(tx_sum_usd/time_diff_days))
 
 print()
 
-print('30 day total issuance (USD): ${}'.format(issuance_sum_usd))
-print('30 day average issuance (USD): ${}'.format(issuance_sum_usd/30))
+print('30 day total issuance (SOL): {:,}'.format(issuance_sum))
+print('30 day average issuance (SOL): {:,}'.format(
+    issuance_sum/time_diff_days))
+
+print()
+
+print('30 day total issuance (USD): ${:,}'.format(issuance_sum_usd))
+print('30 day average issuance (USD): ${:,}'.format(
+    issuance_sum_usd/time_diff_days))
